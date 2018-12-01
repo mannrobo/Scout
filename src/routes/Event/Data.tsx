0ba3e68eb7dd39239038a08310b84ea50c32222d
MayorMonty <artificalchamber@gmail.com>
2018-12-01 00:16:29 -0500

    Add match and ranking data

--- src/routes/Event/Data.tsx
+++ src/routes/Event/Data.tsx
@@ -1,21 +1,56 @@
 import * as React from "react";
 // import VexDB from "src/components/VexDB";
-// import * as vexdb from "vexdb";
-// import { EventsResponseObject } from "vexdb/out/constants/ResponseObjects";
-// import { List } from "antd";
+import * as vexdb from "vexdb";
+import {
+  EventsResponseObject,
+  RankingsResponseObject,
+  MatchesResponseObject
+} from "vexdb/out/constants/ResponseObjects";
+
+import VexDB from "src/components/VexDB";
+import { List, Row, Col } from "antd";
 
 export default (sku: string) =>
   class EventData extends React.Component {
-    async componentWillMount() {}
+    state = {
+      event: {} as EventsResponseObject
+    };
+
+    async componentWillMount() {
+      this.setState({
+        event: (await vexdb.get("events" as any, { sku: sku }))[0]
+      });
+    }
 
     render() {
       return (
-        <p>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis
-          repellendus totam amet illum maiores distinctio reprehenderit mollitia
-          iure, vel officia quidem explicabo. Dolorem error sint sapiente ullam.
-          Itaque, voluptatibus officiis.
-        </p>
+        <Row gutter={16}>
+          <Col md={24} lg={12}>
+            <VexDB
+              endpoint="rankings"
+              args={{ sku }}
+              header={<strong>Rankings</strong>}
+              render={(rank: RankingsResponseObject) => (
+                <List.Item>
+                  {rank.rank}. {rank.team} ({rank.wins}-{rank.losses}-
+                  {rank.ties})
+                </List.Item>
+              )}
+            />
+          </Col>
+          <Col md={24} lg={12}>
+            <VexDB
+              endpoint="matches"
+              args={{ sku }}
+              header={<strong>Matches</strong>}
+              render={(match: MatchesResponseObject) => (
+                <List.Item>
+                  {match.round} {match.instance} {match.matchnum}
+                </List.Item>
+              )}
+            />
+          </Col>
+        </Row>
       );
     }
   };
