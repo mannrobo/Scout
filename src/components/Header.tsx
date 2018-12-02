36e3983cc4d6ac2544af9ab66c0554f0a9e4123d
MayorMonty <artificalchamber@gmail.com>
2018-12-02 17:16:52 -0500

    Add responsive menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,71 @@
-import { Layout } from "antd";
+import { Layout, Menu, Icon } from "antd";
 import * as React from "react";
 import { Link } from "react-router-dom";
+import * as firebase from "firebase";
 
-const Header = Layout.Header;
+export default class Header extends React.Component {
+  state = {
+    user: null as null | firebase.User
+  };
 
-export default () => (
-  <Header id="header">
-    <Link to="/">
-      <img src="/assets/logo.svg" alt="Mann Robotics" className="logo" />
-    </Link>
-    <span className="product">Scout</span>
-  </Header>
-);
+  componentWillMount() {
+    firebase.auth().onAuthStateChanged(user => this.setState({ user }));
+  }
+
+  render() {
+    return (
+      <Layout.Header
+        id="header"
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+          height: "unset",
+          paddingRight: 32
+        }}
+      >
+        <div>
+          <Link to="/">
+            <img src="/assets/logo.svg" alt="Mann Robotics" className="logo" />
+          </Link>
+          <span className="product">Scout</span>
+        </div>
+
+        {this.state.user ? (
+          <Menu
+            mode={window.innerWidth > 550 ? "horizontal" : "inline"}
+            selectedKeys={[]}
+            style={{ borderWidth: 0 }}
+          >
+            <Menu.SubMenu
+              title={
+                <span className="submenu-title-wrapper">
+                  <Icon type="user" />
+                  {this.state.user.displayName}
+                </span>
+              }
+            >
+              <Menu.Item key="user:profile">
+                <Link to="/profile">My Profile</Link>
+              </Menu.Item>
+            </Menu.SubMenu>
+            <Menu.SubMenu
+              title={
+                <span className="submenu-title-wrapper">
+                  <Icon type="team" />
+                  My Team
+                </span>
+              }
+            >
+              <Menu.Item key="team:profile">
+                <Link to="/profile">Team Profile</Link>
+              </Menu.Item>
+            </Menu.SubMenu>
+          </Menu>
+        ) : (
+          <Link to="/login">Log In</Link>
+        )}
+      </Layout.Header>
+    );
+  }
+}
