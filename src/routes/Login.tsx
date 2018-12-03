965da6f4670cbd1f17c4d1cca5bdb147483de23b
MayorMonty <artificalchamber@gmail.com>
2018-12-02 21:17:40 -0500

    Clean up Login Page

--- src/routes/Login.tsx
+++ src/routes/Login.tsx
@@ -5,17 +5,14 @@ import * as firebase from "firebase";
 
 // Configure FirebaseUI.
 const config = {
-  // Popup signin flow rather than redirect flow.
   signInFlow: "popup",
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: "/",
-  // We will display Google and Facebook as auth providers.
   signInOptions: [
     firebase.auth.EmailAuthProvider.PROVIDER_ID,
     firebase.auth.PhoneAuthProvider.PROVIDER_ID,
     firebase.auth.GoogleAuthProvider.PROVIDER_ID,
     firebase.auth.TwitterAuthProvider.PROVIDER_ID
-  ]
+  ],
+  signInSuccessUrl: "/"
 };
 
 interface LoginProps {
