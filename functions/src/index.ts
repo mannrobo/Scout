557e77bd52738d8a436d350ca929d33009d513a4
MayorMonty <artificalchamber@gmail.com>
2018-12-03 23:51:06 -0500

    Firebase function

---
+++ functions/src/index.ts
@@ -0,0 +1,20 @@
+import * as functions from "firebase-functions";
+import * as admin from "firebase-admin";
+
+admin.initializeApp(functions.config().firebase);
+
+const newUserProcedure = functions.auth.user().onCreate(user => {
+  // Create User Profile
+  const profile = admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid);
+
+  profile.set({
+    name: user.displayName || user.email || user.phoneNumber,
+    profile: user.photoURL,
+    team: ""
+  });
+});
+
+export { newUserProcedure };
