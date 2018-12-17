a92448dcfab14babe78b33727e66d9e96d813bdd
MayorMonty <artificalchamber@gmail.com>
2018-12-17 17:51:02 -0500

    Comment Component

--- src/routes/Event/Notes.tsx
+++ src/routes/Event/Notes.tsx
@@ -1,17 +1,40 @@
 import * as React from "react";
 // import VexDB from "src/components/VexDB";
-// import { List } from "antd";
+import { Row, Col, List, Input, Icon, Avatar, Button } from "antd";
 
 export default (sku: string, divison: string) =>
   class EventNotes extends React.Component {
     render() {
       return (
-        <p>
-          Notes is coming, be patient! Lorem ipsum dolor sit, amet consectetur
-          adipisicing elit. At nesciunt earum quas sint, soluta natus porro,
-          modi non consequuntur est quisquam magni molestiae, ipsam animi illum
-          voluptatem dolorem mollitia iure!
-        </p>
+        <Row>
+          <Col lg={12} md={24}>
+            <List
+              header={<strong>Event Notes</strong>}
+              footer={
+                <Input
+                  placeholder="Leave a Note"
+                  suffix={<Icon type="send" />}
+                />
+              }
+              bordered
+              renderItem={(item: string) => (
+                <List.Item>
+                  <List.Item.Meta
+                    style={{ flex: 10 }}
+                    avatar={
+                      <Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />
+                    }
+                    title={<a href="https://ant.design">Brendan McGuire</a>}
+                    description="Ant Design, a design language for background applications, is refined by Ant UED Team"
+                  />
+                  <Button icon="delete" shape="circle" />
+                </List.Item>
+              )}
+              dataSource={[""]}
+            />
+          </Col>
+          <Col lg={12} md={24} />
+        </Row>
       );
     }
   };
